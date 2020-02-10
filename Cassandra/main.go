26e851f44e16c44a5994d63f59f85bf56a84e9f0
Karine Valença <valenca.karine@gmail.com>
2020-02-10 09:30:03 -0300

    inserting data to database

--- Cassandra/main.go
+++ Cassandra/main.go
@@ -14,12 +14,58 @@ func init() {
 	var err error
 
 	cluster := gocql.NewCluster("cassandra")
-	cluster.Keyspace = "name"
-	cluster.ProtoVersion = 3
-	cluster.Consistency = gocql.One
+	
 	Session, err = cluster.CreateSession()
 	if err != nil {
 		panic(err)
 	}
+	var errs []string
+	var created bool = false
+	if err := Session.Query(`
+		CREATE KEYSPACE example WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};`).Exec(); err != nil {
+		errs = append(errs, err.Error())
+	} 
+	cluster.Keyspace = "name"
+	cluster.ProtoVersion = 3
+	cluster.Consistency = gocql.One
+	
+	if err := Session.Query(`
+		create table example.tweet(timeline text, id UUID, text text, PRIMARY KEY(id));`).Exec(); err != nil {
+		errs = append(errs, err.Error())
+	} 
+	
+	if err := Session.Query(`
+		create index on example.tweet(timeline);`).Exec(); err != nil {
+		errs = append(errs, err.Error())
+	} else {
+		created = true
+	}
+
+	if created {
+		fmt.Println("SUCESS")
+	}else{
+		fmt.Println("errors", errs)
+	}
 	fmt.Println("cassandra init done")
+
+}
+
+func InsertUser() {
+	var gocqlUuid gocql.UUID
+	var errs []string
+
+	var created bool = false
+	if err := Session.Query(`
+      INSERT INTO example.tweet (timeline, id, text) VALUES (?, ?, ?)`,
+	  "me", gocqlUuid, "hello world").Exec(); err != nil {
+      errs = append(errs, err.Error())
+    } else {
+      created = true
+	}
+	
+	if created {
+		fmt.Println("SUCESS")
+	}else{
+		fmt.Println("errors", errs)
+	}
 }
--- main.go
+++ main.go
@@ -5,23 +5,9 @@ import (
 )
 
 func main()  {
-	
-
-	/*var err error
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.ProtoVersion = 3
-    cluster.Keyspace = "name"
-    cluster.Consistency = gocql.Quorum
-	session, err := cluster.CreateSession()
-	
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	fmt.Println("cassandra init done")
-	fmt.Printf("hello world")*/
-
 	CassandraSession := Cassandra.Session
-	defer CassandraSession.Close()
 	
+	defer CassandraSession.Close()
+
+	Cassandra.InsertTwitter()
 }
